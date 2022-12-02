f53df90c5e4ee16cca67b1c85406b274de70e862
Natasha <natasha.d.moore@seattlecolleges.edu>
2022-12-01 22:12:26 -0800

    css error correct

--- website/includes/header.php
+++ website/includes/header.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
-    <link rel="stylesheet" href="css/styles.css" type="text/css"/>
-    <link rel="stylesheet" href="css/form.css" type="text/css"/>
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <link rel="stylesheet" href="css/form.css" type="text/css">
 
  </head>
     <body class="<?php echo $body; ?>">
