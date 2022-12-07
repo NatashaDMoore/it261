c70295698a8f719dfcc3610f80018c33405a6b7b
Natasha <natasha.d.moore@seattlecolleges.edu>
2022-12-07 15:48:02 -0800

    database start

--- weeks/week8/credentials.php
+++ weeks/week8/credentials.php
@@ -1,3 +1,8 @@
 <?php
 // natasore_seattle_central  - Database
 // natasore_seattle_central_user - User
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'natasore_seattle_central');
+define('DB_USER', 'natasore_seattle_central_user');
+define('DB_PASSWORD', 'MAlbright37');
---
+++ weeks/week8/people.php
@@ -0,0 +1,40 @@
+<?php
+
+include('config.php');
+
+$sql = 'SELECT * FROM people';
+// must now connect to database
+
+$iConn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die(myError(__FILE__,__LINE__,mysqli_connect_error()));
+// created a variable ($iConn) and assigned the mysqli() function to it.
+// the function connects to my database using credentials.php included in the config.php file which is included above.
+// OR... if we cannot connect to the database... we DIE
+
+
+$result = mysqli_queri($iConn,$sql) or die(myError(__FILE__,__LINE__,mysqli_error($iConn)));
+// query(what DB?, What table?);
+// OR... error
+
+
+// We are asking if the number of rows is greater than 0
+if(mysqli_num_rows($result) >0) {
+    // read the rows using a 'while loop'
+    while($row = mysqli_fetch_assoc($result)) {
+        // $result is the row
+        // assoc refers to the associative array that makes up the row
+
+        echo '
+        <h2>Information About: '.$row['first_name'].'</h2>
+        <ul>
+            <li><b>First Name:</b>'.$row['first_name'].'</li>
+            <li><b>Last Name:</b>'.$row['last_name'].'</li>
+            <li><b>Birthdate:</b>'.$row['birthdate'].'</li>
+        </ul>
+        <p>Click for more information about <a href="">'.$row['first_name'].'</a></p>
+        
+        ';
+
+    } // end while
+
+
+} // end if number of rows is greater than 0
