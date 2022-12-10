9e290d197e21e1751972867a7dec783059a9c627
Natasha <natasha.d.moore@seattlecolleges.edu>
2022-12-09 17:11:34 -0800

    about page complete

---
+++ website/about.php
@@ -0,0 +1,13 @@
+<?php
+include('config.php');
+include('./includes/header.php');
+?>
+<div class="wrapper">
+<h2>My Film Table</h2>
+<a href="./images/film_table2.png"><img src="./images/film_table2.png" alt="" style="max-width:400px;"></a>
+<a href="./images/film_table.png"><img src="./images/film_table.png" alt=""></a>
+</div>
+
+<?php
+include('./includes/footer.php');
+?>
